47b83bee84ed53ef813f80723d9420b4ffaca6d0
Meng Zhuo <mengzhuo1203@gmail.com>
2015-08-13 21:30:19 +0800

    WIP change id to string :(

--- node.go
+++ node.go
@@ -3,39 +3,28 @@ package onehop
 
 import (
 	"fmt"
-	"math/big"
 	"net"
 	"time"
 )
 
 type Event struct {
-	ID     *big.Int  `json:"i"`
+	ID     string    `json:"i"`
 	Time   time.Time `json:"t"`
 	Status uint8     `json:"s"`
 	Addr   string    `json:"a"`
 }
 
 func (e *Event) String() string {
-	return fmt.Sprintf("Event:%x S:%x A:%s T:%s",
+	return fmt.Sprintf("Event:%s S:%x A:%s T:%s",
 		e.ID, e.Status, e.Addr, e.Time)
 }
 
-func (self *Event) ToNode() *Node {
-
-	addr, err := net.ResolveUDPAddr("udp", self.Addr)
-	if err != nil {
-		return nil
-	}
-	return &Node{ID: self.ID, Addr: addr, updateAt: time.Now()}
-
-}
-
 type Node struct {
-	ID       *big.Int
+	ID       string
 	Addr     *net.UDPAddr
 	updateAt time.Time
 }
 
 func (n *Node) String() string {
-	return fmt.Sprintf("ID:%016X Addr:%s", n.ID, n.Addr)
+	return fmt.Sprintf("ID:%s Addr:%s", n.ID, n.Addr)
 }
--- slice.go
+++ slice.go
@@ -1,24 +1,24 @@
 package onehop
 
 import (
-	"math/big"
 	"sort"
+	"sync"
 
 	"github.com/golang/glog"
 )
 
 type Slice struct {
-	Leader *Node
-	Min    *big.Int
-	Max    *big.Int
-	nodes  ByID
+	Min   string
+	Max   string
+	Nodes ByNodeID
+	*sync.Mutex
 }
 
 func (s *Slice) Len() int {
 	return len(s.nodes)
 }
 
-func (s *Slice) successorOf(id *big.Int) (n *Node) {
+func (s *Slice) successorOf(id string) (n *Node) {
 
 	if s.Len() == 0 {
 		// Faster query
@@ -26,23 +26,14 @@ func (s *Slice) successorOf(id *big.Int) (n *Node) {
 	}
 
 	i := s.getID(id)
-	if i == s.Len() {
+
+	if i >= s.Len()-1 {
 		return nil
 	}
-
-	if s.nodes[i].ID.Cmp(id) == 0 {
-		// ID in our nodes
-		if i+1 < s.Len() {
-			// ID in our nodes
-			return s.nodes[i+1]
-		} else {
-			return nil
-		}
-	}
-	return s.nodes[i]
+	return s.nodes[i+1]
 }
 
-func (s *Slice) predecessorOf(id *big.Int) (n *Node) {
+func (s *Slice) predecessorOf(id string) (n *Node) {
 
 	if s.Len() == 0 {
 		// Faster query
@@ -50,78 +41,82 @@ func (s *Slice) predecessorOf(id *big.Int) (n *Node) {
 	}
 
 	i := s.getID(id)
-	if i >= s.Len() {
-		return nil
-	}
 
-	if s.nodes[i].ID.Cmp(id) == 0 {
-		// ID in our nodes
-		if i-1 >= 0 {
-			// ID in our nodes
-			return s.nodes[i-1]
-		} else {
-			return nil
-		}
+	if i != 0 {
+		return s.Nodes[i-1]
 	}
-	return s.nodes[i]
-
+	return nil
 }
-func (u *Slice) getID(id *big.Int) (i int) {
+
+func (u *Slice) getID(id string) (i int) {
 
 	i = sort.Search(len(u.nodes),
 		func(i int) bool {
-			return u.nodes[i].ID.Cmp(id) >= 0
+			return u.nodes[i] >= id
 		})
 	return i
 }
-func (u *Slice) Get(id *big.Int) (n *Node) {
+func (u *Slice) Get(id string) (n *Node) {
 
 	i := u.getID(id)
-	if i < len(u.nodes) && u.nodes[i].ID.Cmp(id) == 0 {
+	if i < len(u.nodes) && u.nodes[i].ID == id {
 		// ID in our nodes
 		return u.nodes[i]
 	}
 	return nil
 }
 
-func (u *Slice) updateLeader() {
-	if u.Len() > 0 {
-		u.Leader = u.nodes[u.Len()/2]
-		return
+func (u *Slice) Leader() *Node {
+
+	if len(u.nodes) > 0 {
+		return u.nodes[len(u.nodes)/2]
 	}
-	u.Leader = nil
+
+	return nil
 }
 
-func (u *Slice) Delete(id *big.Int) bool {
+func (s *Slice) Delete(id string) bool {
+
+	s.Lock()
+	defer s.Unlock()
 
 	i := u.getID(id)
 
-	if i < len(u.nodes) && u.nodes[i].ID.Cmp(id) == 0 {
-		u.nodes = append(u.nodes[:i], u.nodes[i+1:]...)
-		u.updateLeader()
-		glog.Infof("slice %x Delete %x", u.Max, id)
+	if i < len(s.nodes) && s.nodes[i].ID == id {
+		s.nodes = append(s.nodes[:i], s.nodes[i+1:]...)
+		glog.Infof("slice %s Delete %s", s.Max, id)
 		return true
 	}
 	return false
 }
 
-func (u *Slice) add(n *Node) bool {
+func (s *Slice) Add(n *Node) bool {
 
-	if n.ID.Cmp(u.Min) < 0 || n.ID.Cmp(u.Max) > 0 {
+	s.Lock()
+	defer s.Unlock()
+
+	if n.ID < s.Min || n.ID > s.Max {
 		return false
 	}
-	if node := u.Get(n.ID); node == nil {
-		glog.Infof("Unit %x add %x", u.Max, n.ID)
-		u.nodes = append(u.nodes, n)
-		sort.Sort(u.nodes)
-	}
-	u.updateLeader()
 
+	if node := s.Get(n.ID); node == nil {
+		glog.Infof("Slice %s add %s", s.Max, n.ID)
+		s.nodes = append(s.nodes, n)
+		sort.Sort(s.nodes)
+	}
 	return true
 }
 
-type ByID []*Node
+type ByNodeID []*Node
 
-func (b ByID) Len() int           { return len(b) }
-func (b ByID) Swap(i, j int)      { b[i], b[j] = b[j], b[i] }
-func (b ByID) Less(i, j int) bool { return b[i].ID.Cmp(b[j].ID) < 0 }
+func (b ByNodeID) Len() int {
+	return len(b)
+}
+
+func (b ByNodeID) Less(i, j int) bool {
+	return b[i] < b[j]
+}
+
+func (b ByNodeID) Swap(i, j int) {
+	b[i], b[j] = b[j], b[i]
+}
